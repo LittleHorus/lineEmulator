e6ff3919eed7eba51051400384fcd5e3118969b9
LittleHorus <dimonchikgvd@gmail.com>
2021-09-29 12:47:23 +0700

    update xml parser --add to dict data types inner level

--- device_settings_from_xml.py
+++ device_settings_from_xml.py
@@ -53,8 +53,13 @@ class XmlDataLoader():
 						def_view = root[1][i][j].text		
 					if root[1][i][j].tag == 'data_presentation':
 						data_presentation_type = root[1][i][j].attrib['type']
+						data_presentation_dict = {'data_type': '', 'params': ''}
 						print(data_presentation_type, type(data_presentation_type))
 						if data_presentation_type == 'range':
+							range_param_minimum = ''
+							range_param_maximum = ''
+							range_param_step = ''
+							range_param_unit = ''
 							for n in range(len(list(root[1][i][j]))):
 								if root[1][i][j][n].tag == 'minimum':
 									range_param_minimum = root[1][i][j][n].text
@@ -64,12 +69,23 @@ class XmlDataLoader():
 									range_param_step = root[1][i][j][n].text
 								if root[1][i][j][n].tag == 'unit':
 									range_param_unit = root[1][i][j][n].text
+							data_presentation_dict['data_type'] = ('range')
+							data_presentation_dict['params'] = ({'minimum': range_param_minimum,
+																	 'maximum': range_param_maximum,
+																	 'step': range_param_step,
+																	 'unit': range_param_unit})
+
 						if data_presentation_type == 'bytes':
+							bytes_param_encoding = ''
+							bytes_param_max_length = ''
 							for n in range(len(list(root[1][i][j]))):
 								if root[1][i][j][n].tag == 'encoding':
 									bytes_param_encoding = root[1][i][j][n].text
 								if root[1][i][j][n].tag == 'max_length':
 									bytes_param_max_length = root[1][i][j][n].text
+							data_presentation_dict['data_type'] = ('bytes')
+							data_presentation_dict['params'] = ({'encoding': bytes_param_encoding,
+																	 'max_length': bytes_param_max_length})
 						if data_presentation_type == 'enum':
 							enum_param_id = [{'id': '0', 'value': '0', ' text': ''}] * len(list(root[1][i][j]))
 							for n in range(len(list(root[1][i][j]))):
@@ -80,7 +96,9 @@ class XmlDataLoader():
 										enum_param_id[m]['value'] = root[1][i][j][n][m].text
 									if root[1][i][j][n][m].tag == 'text':
 										enum_param_id[m]['text'] = root[1][i][j][n][m].text
-						data_pres = data_presentation_type  # root[1][i][j].text
+							data_presentation_dict['data_type'] = ('enum')
+							data_presentation_dict['params'] = (enum_param_id)
+						data_pres = data_presentation_dict  # root[1][i][j].text
 				self.device_dict['elements'].update({addr: {'alias': att_alias,\
 				 'caption': caption, 'rw': rw, 'description': desc, 'type': type_,\
 				  'default_view': def_view, 'data_presentation': data_pres}})
